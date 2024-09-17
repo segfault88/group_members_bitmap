0d0ac32b2093eec71cf3771e7cdb61b4d78fa25a
Malcolm Lockyer <segfault88@users.noreply.github.com>
2024-09-17 13:44:48 +1200

    first commit

---
+++ go/go.mod
@@ -0,0 +1,11 @@
+module github.com/segfault88/group_members_bitmap
+
+go 1.23.0
+
+require (
+	github.com/RoaringBitmap/roaring v1.9.4 // indirect
+	github.com/RoaringBitmap/roaring/v2 v2.3.4 // indirect
+	github.com/bits-and-blooms/bitset v1.13.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/mschoch/smat v0.2.0 // indirect
+)
---
+++ go/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/RoaringBitmap/roaring/v2"
+	humanize "github.com/dustin/go-humanize"
+)
+
+func main() {
+	input, err := os.Open("../group_members.csv")
+	if err != nil {
+		panic(err)
+	}
+
+	reader := csv.NewReader(input)
+
+	header, err := reader.Read()
+	if err != nil {
+		panic(err)
+	}
+
+	if len(header) != 2 {
+		panic("bad header")
+	}
+
+	fmt.Printf("csv header: %v\n", header)
+
+	start := time.Now()
+
+	bitmaps := map[int]*roaring.Bitmap{}
+
+	total := int64(0)
+
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			break
+		}
+
+		if len(record) != 2 || record[0] == "" || record[1] == "" {
+			fmt.Printf("skipping bad record: %v\n", record)
+			continue
+		}
+
+		total++
+
+		groupID, err := strconv.Atoi(record[0])
+		if err != nil {
+			panic(err)
+		}
+		memberID, err := strconv.Atoi(record[1])
+		if err != nil {
+			panic(err)
+		}
+
+		bitmap, ok := bitmaps[groupID]
+		if !ok {
+			bitmap = roaring.New()
+			bitmaps[groupID] = bitmap
+		}
+		bitmap.Add(uint32(memberID))
+	}
+
+	totalSize := uint64(0)
+
+	for groupID, bitmap := range bitmaps {
+		bitmap.RunOptimize()
+		fmt.Printf("group %d has %s members size %s\n", groupID, humanize.Comma(int64(bitmap.GetCardinality())), humanize.Bytes(bitmap.GetSizeInBytes()))
+
+		totalSize += bitmap.GetSizeInBytes()
+
+		out, err := os.Create(fmt.Sprintf("group_%d.roaring", groupID))
+		if err != nil {
+			panic(err)
+		}
+		_, err = bitmap.WriteTo(out)
+		if err != nil {
+			panic(err)
+		}
+		out.Close()
+	}
+
+	fmt.Printf("done, total: %s group members, total bytes: %s, took: %v\n", humanize.Comma(total), humanize.Bytes(totalSize), time.Since(start))
+}
