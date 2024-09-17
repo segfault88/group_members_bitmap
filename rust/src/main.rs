0ab98b4760ebbd0ecaa1e1aa6cd741090c615dd3
Malcolm Lockyer <segfault88@users.noreply.github.com>
2024-09-17 15:55:04 +1200

    wip

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,29 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    match File::open("../group_members.csv") {
+        Ok(file) => {
+            let mut reader = csv::Reader::from_reader(file);
+            for result in reader.records() {
+                match result {
+                    Ok(record) => {
+                        if record.len() != 2 {
+                            eprintln!("Invalid record: {:?}", record);
+                            continue;
+                        }
+                        let name = &record[0];
+                        let email = &record[1];
+                        println!("Name: {}, Email: {}", name, email);
+                        return;
+                    }
+                    Err(err) => {
+                        eprintln!("Error reading record: {}", err);
+                    }
+                }
+            }
+        }
+        Err(err) => {
+            eprintln!("Error opening file: {}", err);
+        }
+    }
 }
